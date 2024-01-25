c5a2a1882bfa5927ecc872a995a2a23f1ab90307
GustavoAmerico17 <gustavorodrigues.bsb@gmail.com>
2024-01-24 21:29:24 -0300

    corrigindo valor min de sorteio

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ function sortear(){
     const min = document.getElementById("minimo").value
     const max = document.getElementById("maximo").value
 
-    const sorteio = Math.floor(Math.random() * (max - min + 1) + min)
+    
+    const button = document.querySelector(".button-main")
+if(min >= max){
+        alert("O valor minimo tem que ser MENOR que o valor maximo!")
+
+    }
+else{
+        const sorteio = Math.floor(Math.random() * (max - min + 1) + min)
+        
+        alert( sorteio)
+    }
 
     //document.getElementById("resultado").innerHTML= sorteio
-    alert(sorteio)
+    
 }
